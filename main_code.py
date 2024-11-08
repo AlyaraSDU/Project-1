ba7d864c3133e53c66c76ba487029519bf85ed0b
Indemeros <52813441+Indemeros@users.noreply.github.com>
2024-11-08 19:31:03 +0500

    Add Zhandos's code to the main code

--- main_code.py
+++ main_code.py
@@ -197,7 +197,37 @@ count_level = mh_company['Experience'].value_counts(dropna=False)
 
 results = [count_c.head(1), count_level]
 
+# SECTION 4
+# Research question 3:
+# How can parsing IT job listings from Headhunter.kz reveal insights into the most 
+# in-demand skills and technologies in Kazakhstan's tech industry?
 
+# Load the dataset from the provided file
+
+file_path = 'D:job_listings_all_pages.xlsx'
+df = pd.read_excel(file_path)
+
+# Display the first few rows to understand the structure of the data
+
+df.head()
+
+
+# Count the occurrences of each job title to determine demand
+
+job_demand = df['Job Title'].value_counts().reset_index()
+job_demand.columns = ['Job Title', 'Demand']  # Rename columns for clarity
+
+# Sort the data in descending order by demand
+
+job_demand = job_demand.sort_values(by='Demand', ascending=False)
+
+# Save the sorted data to a new Excel file
+
+output_file_path = "C:\\Users\\Zhandos Mukhtarov\\Documents\\it_jobs_demand_sorted.xlsx"
+
+job_demand.to_excel(output_file_path, index=False)
+
+output_file_path
 
 
 
