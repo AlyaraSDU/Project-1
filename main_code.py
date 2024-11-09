11ee2642fc6b8841beddbc6df2c7815601c9c8a3
Indemeros <52813441+Indemeros@users.noreply.github.com>
2024-11-09 21:36:01 +0500

    Refine comments in main code bases to meet the demand of the teacher

--- main_code.py
+++ main_code.py
@@ -1,7 +1,7 @@
 # This is the main code of project 1 
 
 # SECTION 1
-# Parsing the hh.kz website and saving the data into excel file
+# Parse the hh.kz website and save the data into an excel file
 
 !/usr/bin/env python
 coding: utf-8
@@ -13,13 +13,11 @@ import numpy as np
 import pandas as pd
 import time
 
-# Set up the WebDriver (ensure ChromeDriver is installed and in PATH)
 
 driver = webdriver.Chrome()
 
-# Load the initial webpage
-
 base_url = "https://almaty.hh.kz/search/vacancy?L_save_area=true&text=&excluded_text=&professional_role=156&professional_role=160&professional_role=150&professional_role=25&professional_role=165&professional_role=36&professional_role=96&professional_role=104&professional_role=157&professional_role=112&professional_role=113&professional_role=148&professional_role=114&professional_role=116&professional_role=121&professional_role=124&professional_role=125&area=160&salary=&currency_code=KZT&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&hhtmFrom=vacancy_search_filter&page="
+
 
 # Scrape job listings across multiple pages, extracting details like title, 
 # company, experience, salary, and requirements, and store them in a list.
@@ -44,7 +42,7 @@ while True:
 
     for vacancy in vacancies:
         try:
-            # Extracting the job title
+            # Extract the job title
             try:
                 title_element = vacancy.find_element(By.CSS_SELECTOR, "a[data-qa='serp-item__title']")
                 title = title_element.text.strip()
@@ -53,32 +51,32 @@ while True:
                 title = 'Not specified'
                 link = 'Not specified'
 
-            # Extracting the company name
+            # Extract the company name
             try:
                 company_element = vacancy.find_element(By.CSS_SELECTOR, "a[data-qa='vacancy-serp__vacancy-employer']")
                 company = company_element.text.strip()
             except:
                 company = 'Not specified'
 
-            # Extracting experience information
+            # Extract experience information
             try:
                 experience_element = vacancy.find_element(By.CLASS_NAME, "magritte-tag__label___YHV-o_3-0-13").get_attribute("innerHTML")
             except:
                 experience_element = 'Not specified'
 
-            # Extracting salary information
+            # Extract salary information
             try:
                 salary = vacancy.find_element(By.CSS_SELECTOR, "span.magritte-text___pbpft_3-0-15.magritte-text_style-primary___AQ7MW_3-0-15.magritte-text_typography-label-1-regular___pi3R-_3-0-15").text
             except:
                 salary = 'Not specified'
 
-            # Extracting requirement information
+            # Extract requirement information
             try:
                 requirement_element = vacancy.find_element(By.CSS_SELECTOR, "div[data-qa='vacancy-serp__vacancy_snippet_requirement'] > span").text.strip()
             except:
                 requirement_element = 'Not specified'
 
-            # Appending the job information
+            # Append the job information
             jobs.append({
                 'Job Title': title,
                 'Company': company,
@@ -91,12 +89,10 @@ while True:
         except Exception as e:
             print(f"Error while extracting data for a job: {e}")
 
-    # Move to the next page
     current_page += 1
 
-# Close the WebDriver
-
 driver.quit()
+
 
 # Convert to DataFrame and save as Excel
 
@@ -104,7 +100,7 @@ df = pd.DataFrame(jobs)
 print(df)
 df.to_excel('job_listings_all_pages.xlsx', index=False)
 
-print("Data has been saved to job_listings_all_pages.xlsx")
+
 
 # SECTION 2
 # Research question 1:
@@ -116,20 +112,24 @@ df = pd.read_excel('job_listings_all_pages.xlsx')
 df_work = df['Job Title']
 df_work.to_excel('titles.xlsx', index = False)
 
-# Convert every title to lower case and delete missing values
+
+# Convert every title into lowercase and delete missing values
 
 df_work = df_work.str.lower()
 df_work = df_work.dropna()
+
 
 # Select raws that contain words "junior" and "младший"
 
 junior = df_work[df_work.str.contains('junior|младший')]
 junior
 
+
 # Reset old indexing so new indexing will be from 0 to 25
 
 junior = junior.reset_index(drop=True)
 junior.info()
+
 
 # Create a dictionary of key words that target professions
 
@@ -149,53 +149,48 @@ result_dic = {
 }
 result_dic
 
-# Function that merges different descriptions of the same profession into one
 
+# Get series by applying a function that normalizes job titles
+
+# Function that merges different descriptions of the same profession into one
 def normalize_title(title):
     for profession, keywords in result_dic.items():
         if any(keyword in title for keyword in keywords):
             return profession
 
-# Create a new column with normalized job titles
-
 jobs = junior.apply(normalize_title)
+
 
 # Count occurrences of each IT profession and display the result
 
 profession_count = jobs.value_counts()
 print(profession_count)
 
+
+
 # SECTION 3
 # Research question 2:
 # Which company is hiring the most, and what levels of specialization are they hiring?
 
-# Read excel file with parsed data as DataFrame
-
-df = pd.read_excel('Project/job_listings_all_pages.xlsx')
-
 # Count vacancies in each company to get the most hiring one.
 
+df = pd.read_excel('Project/job_listings_all_pages.xlsx')
 count_c = df['Company'].value_counts(dropna=False)
 
-# Save vacancies of the most hiring company to new DF. 
+
+# Save vacancies of the most hiring company to new DF and refine it
 
 mh_company = df[df['Company']=='АО Народный банк Казахстана']
-
-# Change text in Experience column to readable one.
-
 mh_company.loc[mh_company['Experience']=='Опыт более 6&nbsp;лет', 'Experience'] = 'Опыт более 6 лет'
-
-# Write experience levels to a list
-
 experience = mh_company['Experience'].unique().tolist()
+
 
 # Count how many vacancies are there for each experience level
 
 count_level = mh_company['Experience'].value_counts(dropna=False)
-
-# Write all results to one list
-
 results = [count_c.head(1), count_level]
+
+
 
 # SECTION 4
 # Research question 3:
@@ -206,9 +201,6 @@ results = [count_c.head(1), count_level]
 
 file_path = 'D:job_listings_all_pages.xlsx'
 df = pd.read_excel(file_path)
-
-# Display the first few rows to understand the structure of the data
-
 df.head()
 
 
@@ -216,17 +208,13 @@ df.head()
 
 job_demand = df['Job Title'].value_counts().reset_index()
 job_demand.columns = ['Job Title', 'Demand']  # Rename columns for clarity
-
-# Sort the data in descending order by demand
-
 job_demand = job_demand.sort_values(by='Demand', ascending=False)
+
 
 # Save the sorted data to a new Excel file
 
 output_file_path = "C:\\Users\\Zhandos Mukhtarov\\Documents\\it_jobs_demand_sorted.xlsx"
-
 job_demand.to_excel(output_file_path, index=False)
-
 output_file_path
 
 
